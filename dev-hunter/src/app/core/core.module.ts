a3a9672f1c05daf73b2551b266822d2e100ba76c
Hristina Hristova <hristina.hristova@pliant.io>
2022-07-22 11:10:11 +0300

    Basic html and css to header and footer

--- dev-hunter/src/app/core/core.module.ts
+++ dev-hunter/src/app/core/core.module.ts
@@ -3,7 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatButtonModule,
+  MatGridListModule
+];
 
 @NgModule({
   declarations: [
@@ -11,7 +23,12 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, 
+    [...materialModules]
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
