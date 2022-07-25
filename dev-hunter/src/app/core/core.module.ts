e1c1e78ab2745250bbed4effa45e8c10515a8335
Hristina Hristova <hristina.hristova@pliant.io>
2022-07-25 11:23:14 +0300

    Adding menu to header

--- dev-hunter/src/app/core/core.module.ts
+++ dev-hunter/src/app/core/core.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { HomeComponent } from './home/home.component';
-import { CarouselComponent } from './home/carousel/carousel.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 const materialModules = [
   MatToolbarModule,
   MatIconModule,
   MatListModule,
   MatButtonModule,
-  MatGridListModule
+  MatGridListModule,
+  MatMenuModule
 ];
 
 @NgModule({
