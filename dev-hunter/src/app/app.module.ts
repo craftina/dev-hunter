fc8ad8e86ddfd419b98c3078b8dc2bdc93053643
Hristina Hristova <hristina.hristova@pliant.io>
2022-07-25 12:14:26 +0300

    Changing the place of header, footer and home from core to app

--- dev-hunter/src/app/app.module.ts
+++ dev-hunter/src/app/app.module.ts
@@ -4,15 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatButtonModule,
+  MatGridListModule,
+  MatMenuModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule,
+    [...materialModules]
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dev-hunter/src/app/core/core.module.ts
+++ dev-hunter/src/app/core/core.module.ts
@@ -1,41 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { CarouselComponent } from './home/carousel/carousel.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
-
-const materialModules = [
-  MatToolbarModule,
-  MatIconModule,
-  MatListModule,
-  MatButtonModule,
-  MatGridListModule,
-  MatMenuModule
-];
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    CarouselComponent
   ],
   imports: [
-    CommonModule, 
-    [...materialModules]
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent
+    CommonModule
+   
   ]
 })
 export class CoreModule { }
