ccf94d11ac38d6a4efd94016e58002f987f14b81
Hristina Hristova <hristina.hristova@pliant.io>
2022-08-02 18:18:18 +0300

    adding locations component

--- dev-hunter/src/app/main/main.module.ts
+++ dev-hunter/src/app/main/main.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
+import { LocationsComponent } from './components/locations/locations.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    LocationsComponent
   ],
   imports: [
     CommonModule
